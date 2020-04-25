ec6300a28e3229c568f12f2814ee6daaf77d2a05
Tiago-Perri <tiagocnsr@gmail.com>
2020-04-25 00:43:47 -0300

    Implementando JS

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function(){
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js');
+    $body.classList.add('js');
+
+    var menu = new Menu({
+        container: '.nav-header',
+        toggleBtn: '.header__btnMenu',
+        widthEnabled: 426
+    })
+})()
