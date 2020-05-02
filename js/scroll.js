beec98fdc71639789ce3940032bcb8886e65b929
Tiago-Perri <tiagocnsr@gmail.com>
2020-05-02 13:39:45 -0300

    Ajustes scroll.js / img / Links internos

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,46 @@
-$('a').click(function(e){
+$('.nav-header a').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top,
+        menuHeight = $('nav').innerHeight();
+
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 900);
+});
+
+$('.banner-inicial .content-banner a').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top
+
+    $('html, body').animate({
+        scrollTop: targetOffset
+    }, 900);
+});
+
+$('.contact a:last-child').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top,
+        menuHeight = $('nav').innerHeight();
+
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 900);
+});
+
+$('.introduçao-Lex .content-introduçao a').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top
+
+    $('html, body').animate({
+        scrollTop: targetOffset
+    }, 900);
+});
+
+$('.nav-footer a').click(function(e){
     e.preventDefault();
     var id = $(this).attr('href'),
         targetOffset = $(id).offset().top,
